b9eb6e397a5600c34326fe8e9c499daf2e666acb
Pankaj Kumar Mahto <pankajmahto9898@gmail.com>
2023-08-23 16:18:16 +0530

    first commit

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './style.css'
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>@2023 created by : Pankaj Kumar Mahto</p>
+    </footer>
+  );
+}
+
+export default Footer;
